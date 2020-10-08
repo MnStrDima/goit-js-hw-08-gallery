b2ca6021646e8fda18eb3b1f290b6a737ae6d2a0
Dmitry Medychenko <rAv3ndima91@gmail.com>
2020-10-08 22:16:27 +0300

    Подготавливает файлы к работе над ДЗ

---
+++ js/task-1.js
@@ -0,0 +1,19 @@
+import galleryItems from './gallery-items.js';
+
+console.log(galleryItems);
+
+{
+  /* <li class="gallery__item">
+    <a
+        class="gallery__link"
+        href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+    >
+        <img
+            class="gallery__image"
+            src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
+            data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+            alt="Tulips"
+        />
+    </a>
+</li> */
+}

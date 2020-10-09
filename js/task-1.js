1a584f401410fe10425192f03c7bb95e095ea888
Dmitry Medychenko <rAv3ndima91@gmail.com>
2020-10-09 23:29:37 +0300

    Выполняет все обязательные требования к дз и + 2 необязательных, кнопки пролистывания галереи в процессе

--- js/task-1.js
+++ js/task-1.js
@@ -1,19 +1,64 @@
 import galleryItems from './gallery-items.js';
 
-console.log(galleryItems);
+const galleryListRef = document.querySelector('.js-gallery');
+const galleryCardsMarkup = createGalleryCardsMarkup(galleryItems);
+const lightboxRef = document.querySelector('.js-lightbox');
+const lightboxImageRef = document.querySelector('.lightbox__image');
 
-{
-  /* <li class="gallery__item">
-    <a
-        class="gallery__link"
-        href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-    >
+galleryListRef.insertAdjacentHTML('beforeend', galleryCardsMarkup);
+
+function createGalleryCardsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+        <a class="gallery__link"
+        href="${original}"
+        >
         <img
             class="gallery__image"
-            src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-            data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-            alt="Tulips"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
         />
     </a>
-</li> */
+</li>
+`;
+    })
+    .join('');
+}
+
+galleryListRef.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(e) {
+  e.preventDefault();
+  onOpenModal(e);
+}
+
+function onOpenModal(e) {
+  window.addEventListener('keydown', onEscCloseModal);
+  lightboxRef.classList.add('is-open');
+  lightboxImageRef.src = e.target.dataset.source;
+  lightboxImageRef.alt = e.target.alt;
+}
+
+lightboxRef.addEventListener('click', onOverlayClickCloseModal);
+
+function onCloseModal() {
+  window.removeEventListener('keydown', onEscCloseModal);
+  lightboxRef.classList.remove('is-open');
+  lightboxImageRef.src = '';
+  lightboxImageRef.alt = '';
+}
+
+function onOverlayClickCloseModal(e) {
+  if (e.target !== lightboxImageRef) {
+    onCloseModal();
+  }
+}
+
+function onEscCloseModal(e) {
+  if (e.code === 'Escape') {
+    onCloseModal();
+  }
 }
